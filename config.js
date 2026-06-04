3f7f21ef3243ad6c81a2e83f04aefc494ada4fee
kimyoungun90-beep <kim.youngun90@gmail.com>
2026-06-04 16:08:58 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Supabase 연결 설정 파일
+// 아래 2개 값만 본인 Supabase 프로젝트 값으로 바꾸면 공유 모드가 작동합니다.
+// 예)
+// window.JEJU_SUPABASE_URL = "https://xxxxx.supabase.co";
+// window.JEJU_SUPABASE_ANON_KEY = "sb_publishable_xxxxx";
+
+window.JEJU_SUPABASE_URL = "";
+window.JEJU_SUPABASE_ANON_KEY = "";
